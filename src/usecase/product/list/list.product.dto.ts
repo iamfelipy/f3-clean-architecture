98c4a5c97204add3123ad5c3bf30adcc120432e6
felipy <felipy81br@gmail.com>
2025-12-28 15:08:34 -0300

    feat: add list product usecase with tests

---
+++ src/usecase/product/list/list.product.dto.ts
@@ -0,0 +1,10 @@
+type Product = {
+  id: string
+  name: string
+  price: number
+}
+
+export interface InputListProductDTO {}
+export interface OutputListProductDTO {
+  products: Product[]
+}
---
+++ src/usecase/product/list/list.product.integration.spec.ts
@@ -0,0 +1,49 @@
+import { Sequelize } from "sequelize-typescript"
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository"
+import { ListProductUseCase } from "./list.product.usecase"
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model"
+import Product from "../../../domain/product/entity/product"
+
+describe("integration test: list products use case", () => {
+  let sequelize: Sequelize
+  
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+  
+  it("should be able to list products", async () => {
+    const productRepository = new ProductRepository()
+    const usecase = new ListProductUseCase(productRepository)
+
+    const product1 = new Product("123", "banana", 123);
+    await productRepository.create(product1);
+
+    const product2 = new Product("124", "uva", 456);
+    await productRepository.create(product2);
+
+    const input = {}
+    
+    const output = await usecase.execute(input)
+
+    expect(output.products.length).toBe(2);
+    expect(output.products[0].id).toBe(product1.id);
+    expect(output.products[0].name).toBe(product1.name);
+    expect(output.products[0].price).toBe(product1.price);
+    expect(output.products[1].id).toBe(product2.id);
+    expect(output.products[1].name).toBe(product2.name);
+    expect(output.products[1].price).toBe(product2.price);
+    
+  })
+})
